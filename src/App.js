057ada90f0be82bca0e7ab3a6182f5969979ae0f
michaelleriche <mikeleriche@gmail.com>
2020-05-15 17:06:14 -0400

    fizzbuzz work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>fizzbuzz</p>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        counter: 0
+    }
+  }
+  upVote = () => {
+      const addToVote = this.state.counter;
+      this.setState({
+          counter: addToVote + 1,
+      })
+  }
+  downVote = () => {
+    const downVote = this.state.counter;
+    this.setState({
+        counter: downVote - 1,
+    })
 }
+  fizzBuzz = () => {
+    if (this.state.counter % 3 === 0) {
+      return 'fizz';
+    } else if (this.state.counter % 5 === 0) {
+      return 'buzz';
+    } else if (this.state.counter % 3 === 0 && this.state.counter % 5 === 0) {
+      return 'fizzbuzz';
+    }
+  }
+
+  render() {
+    return (
+        <div>
+           <p>Number: {this.state.counter}</p>
+           {this.state.counter % 3 === 0 ? <p>fizz</p> : null}
+           {this.state.counter % 5 === 0 ? <p>buzz</p> : null}
+           {this.state.counter % 3 === 0 && this.state.counter % 5 === 0 ? <p>fizzbuzz</p> : null}
+           
+
+            <button onClick={this.upVote}>upVote</button>
+            <button onClick={this.downVote}>downVote</button>
+        </div>
+    ) 
+  }
+}
+
+
+
+
 
 export default App;
